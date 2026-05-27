d2b4ebae46e240ec8d53e5651cd7416536fe0ec3
Jon Hauge <116887188+jonaahauge@users.noreply.github.com>
2026-05-27 11:33:40 +0200

    ..

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 from ultralytics import YOLO
 
+def main():
+    model = YOLO("yolo26n.pt")
+
+    results = model.train(
+        data="data.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=16,
+        name="animals_v1",    
+    )
+    print(results.save_dir)
+
+if __name__ == "__main__":
+    main()
+
+main
